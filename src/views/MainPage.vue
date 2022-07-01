ca3e1fe4e65c4cb5b55156b4b86149cdd8f6ff21
Nastya Borisova <nastasiia306@gmail.com>
2022-07-01 12:59:26 +0300

    [feat]: create slider component

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <SliderComponent />
     <ArticlesBlock />
   </div>
 </template>
 
 <script>
 import ArticlesBlock from '@/components/main/ArticlesBlock';
+import SliderComponent from '@/components/main/SliderComponent';
 
 export default {
   name: 'MainPage',
   components: {
     ArticlesBlock,
+    SliderComponent,
   },
 }
 </script>
